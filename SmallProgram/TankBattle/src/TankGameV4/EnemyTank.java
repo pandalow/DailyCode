8d22d667a6ad310085566989419164792598734d
pandalow <zxj000hugh@gmail.com>
2024-03-05 15:44:09 +0000

    1) Adding multiple tanks; 2) Making tanks random move; 3) Adding explosion effect

--- SmallProgram/TankBattle/src/TankGameV4/EnemyTank.java
+++ SmallProgram/TankBattle/src/TankGameV4/EnemyTank.java
@@ -1,25 +1,83 @@
 package TankGameV4;
 
+import java.util.Map;
 import java.util.Vector;
 
-public class EnemyTank extends Tank{
+public class EnemyTank extends Tank implements Runnable {
     Vector<Bullet> bullets = new Vector<>();
-    Bullet bullet  = null;
-    public EnemyTank(int x, int y){
-        super(x,y);
+    Bullet bullet = null;
+
+    public EnemyTank(int x, int y) {
+        super(x, y);
     }
+
     boolean isLive = true;
-    public void shotBullet(){
-//        while(true){
-        bullet = new Bullet(getX()+20,getY()+60,2);
+
+    public void shotBullet() {
+
+        bullet = new Bullet(getX() + 20, getY() + 60, 2);
         bullets.add(bullet);
         Thread thread = new Thread(bullet);
         thread.start();
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
+    }
+
+    public void run() {
+        while (true) {
+            switch (getDirect()) {
+                case 0:
+                    for (int i = 0; i < 30; i++) {
+                        if (getY() > 0) {
+                            moveUp();
+                        }
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    break;
+
+                case 1:
+                    for (int i = 0; i < 30; i++) {
+                        if (getX() + 60 < 1000) {
+                            moveRight();
+                        }
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < 30; i++) {
+                        if (getY() + 60 < 750) {
+                            moveDown();
+                        }
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < 30; i++) {
+                        if (getX() > 0) {
+                            moveLeft();
+                        }
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    break;
+            }
+            setDirect((int) (Math.random() * 4));
+            if (!isLive) {
+                break;
+            }
+        }
     }
 }
